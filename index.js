b0fc8ef2343e191a7dbefe5606e663b987212c0d
Balasaraswathi11 <twilightestrella11@gmail.com>
2024-07-16 07:42:00 +0530

    commit

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ app.use(cors());
 const PORT = 4000;
 
 app.get("/first", (req, res) => {
-    res.status(200).json({ message: "welcome" }); // Corrected from req.status to res.status
+    // res.status(200).json({ message: "welcome" }); // Corrected from req.status to res.status
+    res.status(200).send(
+      `<div style="background-color:blue: ; color:white">
+      <h1>hey pal, welcome</h1>
+      </div>`)
 });
 
 app.listen(PORT, () => {
